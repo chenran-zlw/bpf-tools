7664e84c4629a2438ce61b81822a7addbae57cb3
chenran-zlw <chenran.zlw@alibaba-inc.com>
2026-02-27 23:46:39 +0800

    Update show_tcp_latency.bpf.c

--- show_tcp_latency/show_tcp_latency.bpf.c
+++ show_tcp_latency/show_tcp_latency.bpf.c
@@ -19,55 +19,37 @@ struct {
 
 static __always_inline bool match_ports(u16 sport, u16 dport) {
     if (target_rx_port == 0 && target_tx_port == 0) return true;
-    
-    if (target_rx_port != 0) {
-        if (sport == target_rx_port || dport == target_rx_port) return true;
-    }
-    if (target_tx_port != 0) {
-        if (sport == target_tx_port || dport == target_tx_port) return true;
-    }
+    if (target_rx_port != 0 && (sport == target_rx_port || dport == target_rx_port)) return true;
+    if (target_tx_port != 0 && (sport == target_tx_port || dport == target_tx_port)) return true;
     return false;
-}
-
-static __always_inline int submit_event(void *ctx, struct event_t *e) {
-    e->ts = bpf_ktime_get_ns();
-    bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, e, sizeof(*e));
-    return 0;
 }
 
 static __always_inline int process_skb(void *ctx, struct sk_buff *skb, u32 type) {
     if (!skb) return 0;
-
-    __u16 protocol = BPF_CORE_READ(skb, protocol);
-    if (protocol != bpf_htons(ETH_P_IP)) return 0;
+    if (BPF_CORE_READ(skb, protocol) != bpf_htons(ETH_P_IP)) return 0;
 
     unsigned char *head = BPF_CORE_READ(skb, head);
-    __u16 network_header = BPF_CORE_READ(skb, network_header);
+    __u16 nh = BPF_CORE_READ(skb, network_header);
     
     struct iphdr iph;
-    if (bpf_probe_read_kernel(&iph, sizeof(iph), head + network_header) < 0) return 0;
+    if (bpf_probe_read_kernel(&iph, sizeof(iph), head + nh) < 0) return 0;
     if (iph.protocol != IPPROTO_TCP) return 0;
 
-    u32 ip_hlen = iph.ihl << 2;
     struct tcphdr tcph;
-    if (bpf_probe_read_kernel(&tcph, sizeof(tcph), head + network_header + ip_hlen) < 0) return 0;
+    if (bpf_probe_read_kernel(&tcph, sizeof(tcph), head + nh + (iph.ihl << 2)) < 0) return 0;
 
     u16 src = bpf_ntohs(tcph.source);
     u16 dst = bpf_ntohs(tcph.dest);
-    
     if (!match_ports(src, dst)) return 0;
 
     struct event_t e = {
-        .type = type,
-        .saddr = iph.saddr,
-        .daddr = iph.daddr,
-        .sport = src,
-        .dport = dst,
-        .seq = bpf_ntohl(tcph.seq),
-        .ack_seq = bpf_ntohl(tcph.ack_seq)
+        .type = type, .saddr = iph.saddr, .daddr = iph.daddr,
+        .sport = src, .dport = dst,
+        .seq = bpf_ntohl(tcph.seq), .ack_seq = bpf_ntohl(tcph.ack_seq),
+        .ts = bpf_ktime_get_ns()
     };
-
-    return submit_event(ctx, &e);
+    bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &e, sizeof(e));
+    return 0;
 }
 
 SEC("tracepoint/net/netif_receive_skb")
@@ -85,27 +67,27 @@ int kprobe__tcp_v4_do_rcv(struct pt_regs *ctx) {
     return process_skb(ctx, (struct sk_buff *)PT_REGS_PARM2_CORE(ctx), TYPE_TCP_V4_DO_RCV);
 }
 
+SEC("kprobe/tcp_queue_rcv")
+int kprobe__tcp_queue_rcv(struct pt_regs *ctx) {
+    return process_skb(ctx, (struct sk_buff *)PT_REGS_PARM2_CORE(ctx), TYPE_TCP_QUEUE_RCV);
+}
+
 SEC("kprobe/sock_def_readable")
 int kprobe__sock_def_readable(struct pt_regs *ctx) {
     struct sock *sk = (struct sock *)PT_REGS_PARM1_CORE(ctx);
     struct sock_common skc;
     bpf_probe_read_kernel(&skc, sizeof(skc), &sk->__sk_common);
-
-    if (skc.skc_family != AF_INET) return 0; // 只抓 IPv4
-
+    if (skc.skc_family != AF_INET) return 0;
     u16 lport = skc.skc_num;
     u16 rport = bpf_ntohs(skc.skc_dport);
-
     if (!match_ports(lport, rport)) return 0;
 
     struct event_t e = {
-        .type = TYPE_SOCK_DEF_READABLE,
-        .saddr = skc.skc_daddr,
-        .daddr = skc.skc_rcv_saddr,
-        .sport = rport,
-        .dport = lport
+        .type = TYPE_SOCK_DEF_READABLE, .saddr = skc.skc_daddr, .daddr = skc.skc_rcv_saddr,
+        .sport = rport, .dport = lport, .ts = bpf_ktime_get_ns()
     };
-    return submit_event(ctx, &e);
+    bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &e, sizeof(e));
+    return 0;
 }
 
 SEC("kprobe/tcp_cleanup_rbuf")
@@ -114,30 +96,24 @@ int kprobe__tcp_cleanup_rbuf(struct pt_regs *ctx) {
     int copied = (int)PT_REGS_PARM2_CORE(ctx);
     struct sock_common skc;
     bpf_probe_read_kernel(&skc, sizeof(skc), &sk->__sk_common);
-
     if (skc.skc_family != AF_INET || copied <= 0) return 0;
-
     u16 lport = skc.skc_num;
     u16 rport = bpf_ntohs(skc.skc_dport);
     if (!match_ports(lport, rport)) return 0;
 
     struct tcp_sock *ts = (struct tcp_sock *)sk;
-    u32 copied_seq;
-    bpf_probe_read_kernel(&copied_seq, sizeof(copied_seq), &ts->copied_seq);
+    u32 cseq;
+    bpf_probe_read_kernel(&cseq, sizeof(cseq), &ts->copied_seq);
 
     struct event_t e = {
-        .type = TYPE_TCP_CLEANUP_RBUF,
-        .saddr = skc.skc_daddr,
-        .daddr = skc.skc_rcv_saddr,
-        .sport = rport,
-        .dport = lport,
-        .copied = (u32)copied,
-        .copied_seq = copied_seq
+        .type = TYPE_TCP_CLEANUP_RBUF, .saddr = skc.skc_daddr, .daddr = skc.skc_rcv_saddr,
+        .sport = rport, .dport = lport, .copied = (u32)copied, .copied_seq = cseq,
+        .ts = bpf_ktime_get_ns()
     };
-    return submit_event(ctx, &e);
+    bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &e, sizeof(e));
+    return 0;
 }
 
-// --- 发送路径 ---
 SEC("kprobe/ip_output")
 int kprobe__ip_output(struct pt_regs *ctx) {
     return process_skb(ctx, (struct sk_buff *)PT_REGS_PARM3_CORE(ctx), TYPE_IP_OUTPUT);
